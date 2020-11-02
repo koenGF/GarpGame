e399f5ea5a37dd6e4594427596e13445fff2ec47
koenGF <2144264@talnet.nl>
2020-11-02 14:40:08 +0100

    added actor Garp & Gnomus WIP

--- Garp.java
+++ Garp.java
@@ -8,12 +8,51 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Garp extends Actor
 {
+    private GreenfootImage imageLeft;
+    private GreenfootImage imageRight;
+    
+    public Garp() 
+    {
+        imageLeft = new GreenfootImage("GarpLeft.png");
+        imageRight = new GreenfootImage("GarpRight.png");        
+    }
+    
     /**
      * Act - do whatever the Garp wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("right")) {
+            setRotation(0);
+            if(getImage() != imageRight) {
+                setImage(imageRight);
+            }
+            move(5);
+        }
+        
+        if(Greenfoot.isKeyDown("left")) {
+            if(getImage() != imageLeft) {
+                setImage(imageLeft);
+            }
+            setRotation(0);
+            move(-5);
+        }
+        
+        if(Greenfoot.isKeyDown("up")) {
+            if(getImage() != imageRight) {
+                setImage(imageRight);
+            }
+            setRotation(-90);
+            move(5);
+        }
+        
+        if(Greenfoot.isKeyDown("down")) {
+            if(getImage() != imageRight) {
+                setImage(imageRight);
+            }
+            setRotation(90);
+            move(5);
+        }
     }    
 }
---
+++ Gnomus.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Gnomus here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Gnomus extends Actor
+{
+    /**
+     * Act - do whatever the Gnomus wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        int random;
+        move(5);
+        
+    }    
+}
